2524895fe406e97217afd7ae22461b888fabf020
adam terlson <adam.terlson@gmail.com>
2013-11-05 22:37:13 -0600

    Adding different types of climbing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,60 @@
 angular.module('tt', ['ngResource']);
 
+var vScale = [
+	{
+		points: 6,
+		description: 'VB'
+	},
+	{
+		points: 7,
+		description: 'V0'
+	},
+	{
+		points: 8,
+		description: 'V1'
+	},
+	{
+		points: 9,
+		description: 'V2'
+	},
+	{
+		points: 10,
+		description: 'V3'
+	}
+];
+
+var yosemiteScale = [
+	{
+		points: 6,
+		description: '5.6'
+	},
+	{
+		points: 7,
+		description: '5.7'
+	},
+	{
+		points: 8,
+		description: '5.8'
+	},
+	{
+		points: 9,
+		description: '5.9'
+	},
+	{
+		points: 10,
+		description: '5.10'
+	}
+];
+
 function TicklistCtrl($scope, $resource) {
 	var Tick = $resource('api/ticks/:id', { id: '@id' });
 	var ticks = $scope.ticks = Tick.query();
-	$scope.climbs = [
-		{
-			points: 6,
-			description: '5.6'
-		},
-		{
-			points: 7,
-			description: '5.7'
-		},
-		{
-			points: 8,
-			description: '5.8'
-		},
-		{
-			points: 9,
-			description: '5.9'
-		},
-		{
-			points: 10,
-			description: '5.10'
-		},
-	];
+
+	$scope.climbs = {
+		'top rope': yosemiteScale,
+		'sport': yosemiteScale,
+		'bouldering': vScale
+	};
 
 	$scope.totalPoints = function () {
 		var sum = 0;
@@ -41,10 +71,11 @@ function TicklistCtrl($scope, $resource) {
 		return remaining < 0 ? 0 : remaining;
 	};
 
-	$scope.addTick = function (climb) {
-		console.log(climb);
-		if (!climb) return;
+	$scope.addTick = function (climbingType, climb) {
+		if (!climbingType || !climb) return;
 		var tick = new Tick(climb);
+		tick.climbingType = climbingType;
+
 		tick.$save();
 		ticks.push(tick);
 	};
