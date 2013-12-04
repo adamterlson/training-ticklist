045ecbd9ab3d554c2e0963ed3c68eb138b0827f3
adam terlson <adam.terlson@gmail.com>
2013-12-03 21:16:43 -0600

    Workout timer

--- public/javascripts/directives/timer.js
+++ public/javascripts/directives/timer.js
@@ -9,7 +9,12 @@ tt.directive('timer', function ($timeout) {
 		template: '{{seconds | timer}}',
 		link: function ($scope, element, attrs) {
 			var tick = function () {
-				$scope.seconds -= 1;
+				if (attrs.up) {
+					$scope.seconds++;
+				}
+				else {
+					$scope.seconds--;
+				}
 				if ($scope.seconds > 0) {
 					$timeout(tick, 1000);
 				}
