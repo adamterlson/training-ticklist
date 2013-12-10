7a8163cec3ef7138242b1bdea494564308850632
adam terlson <adam.terlson@gmail.com>
2013-12-10 01:20:52 -0600

    Charting directive

--- public/javascripts/directives/collapsable.js
+++ public/javascripts/directives/collapsable.js
@@ -11,3 +11,33 @@ tt.directive('collapsable', function () {
 		}
 	};
 });
+
+
+tt.directive('chart', ['$compile', function ($compile){
+	return {
+		restrict: 'E',
+		scope: { 
+			datapoints: '=datapoints', 
+			sexypoints: '=sexypoints'
+		},
+		link: function($scope, element, attrs) {
+			$scope.$watch('datapoints', function (newValue, oldValue) {
+				$scope.bars = $scope.datapoints.map(function (tick) { 
+					return {
+						width: 500/$scope.datapoints.length - 2,
+						height: tick.points*10, // hard code point for ticks
+						primaryLabel: tick.description,
+						secondaryLabel: tick.points
+					};
+				});
+			}, true);
+
+			var html = [];
+			html.push('<div class="chart-container"><div class="chart">');
+				html.push('<div class="bar" ng-repeat="bar in bars" style="height: {{bar.height}}px; width: {{bar.width}}px"><span class="primary">{{bar.primaryLabel}}</span><span class="secondary">{{bar.secondaryLabel | sexypoints}}</span></div>')
+            html.push("</div></div>")
+            element.html(html.join(''));
+            $compile(element.contents())($scope);
+		}
+	}
+}]);
