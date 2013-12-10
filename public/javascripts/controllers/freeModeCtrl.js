3c3556bb2c4f24454a6a8b9bf5cfba2667f4d81e
adam terlson <adam.terlson@gmail.com>
2013-12-10 03:45:32 -0600

    Changing slope so one can never get zero points

--- public/javascripts/controllers/freeModeCtrl.js
+++ public/javascripts/controllers/freeModeCtrl.js
@@ -62,7 +62,7 @@ function calculatePoints(rating, scale, bestClimb) {
 	var maxPointIndex = bestClimb,
 		bonus = MAX_POINTS/4,
 		index = rating,
-		slope = MAX_POINTS / (SCALE_LENGTH-1);
+		slope = MAX_POINTS / (SCALE_LENGTH);
 
 	if (index > maxPointIndex) {
 		return MAX_POINTS + bonus * (index - maxPointIndex);
